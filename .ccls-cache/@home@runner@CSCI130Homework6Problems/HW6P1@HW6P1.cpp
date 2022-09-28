4b85e797f8f9dc1860fd1f3dfe199e4a7ea08690
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-09-28 15:42:58 +0000

    Game is functioning. Fixing player vs computer win if statement

--- .ccls-cache/@home@runner@CSCI130Homework6Problems/HW6P1@HW6P1.cpp
+++ .ccls-cache/@home@runner@CSCI130Homework6Problems/HW6P1@HW6P1.cpp
@@ -6,28 +6,29 @@
 using namespace std;
 
 char func_computer_choice() {
-  int A = rand() % 4; // chooses a random number between 0 and 5
-  char B;     // preparing an answer
+  srand(time(NULL));
+  int A = rand() % 5; // chooses a random number between 0 and 5
+  char B;             // preparing an answer
 
   switch (A) {
   case 0: // Rock
-    cout << "Rock";
+    cout << "Rock\n";
     B = 'R';
     break;
   case 1: // Paper
-    cout << "Paper";
+    cout << "Paper\n";
     B = 'P';
     break;
   case 2: // Scissors
-    cout << "Scissors";
+    cout << "Scissors\n";
     B = 'S';
     break;
   case 3: // Lizard
-    cout << "Lizard";
+    cout << "Lizard\n";
     B = 'L';
     break;
   case 4: // Spock
-    cout << "Spock";
+    cout << "Spock\n";
     B = 'K';
     break;
   }
@@ -37,7 +38,8 @@ char func_computer_choice() {
 int game_loop() {
 
   char player_choice, R, P, S, L, K; // declare variables
-  string computer_choice;
+  char computer_choice;
+  int x = 1;
   // R = R;
   // P = P;
   // S = S;
@@ -50,142 +52,138 @@ int game_loop() {
   // endl;
 
   cin >> player_choice; // player chooses rock, paper, scissors...
-
-  if ((player_choice != R) and (player_choice != P) and (player_choice != S) and
-      (player_choice != L) and (player_choice != K)) {
-    cout << "this doesn't work. Please try again";
-  }
-
-  if (player_choice == R) {
-    if (computer_choice == "L") // win
+  if (player_choice == 'R') {
+    if (computer_choice == 'L') // win
     {
       cout << "Rock crushes Lizard. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "S") // win
+    if (computer_choice == 'S') // win
     {
       cout << "Rock smashes Scissors. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "P") // loss
+    if (computer_choice == 'P') // loss
     {
       cout << "Paper covers Rock. You Lose..." << endl;
       return 1;
     }
-    if (computer_choice == "K") // loss
+    if (computer_choice == 'K') // loss
     {
       cout << "Spock vaporizes Rock. You Lose..." << endl;
       return 1;
     } else // tie
     {
-      cout << "It's a tie.";
+      cout << "It's a tie." << endl;
+
       return 2;
     }
   }
-  if (player_choice == P) {
-    if (computer_choice == "R") // win
+  if (player_choice == 'P') {
+    if (computer_choice == 'R') // win
     {
       cout << "Paper cover Rock. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "K") // win
+    if (computer_choice == 'K') // win
     {
       cout << "Paper disproves Spock. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "S") // loss
+    if (computer_choice == 'S') // loss
     {
       cout << "Scissors cut Paper. You Lose..." << endl;
       return 1;
     }
-    if (computer_choice == "L") // loss
+    if (computer_choice == 'L') // loss
     {
       cout << "Lizard eats Paper. You Lose..." << endl;
       return 1;
     } else // tie
     {
-      cout << "It's a tie.";
+      cout << "It's a tie." << endl;
       return 2;
     }
   }
-  if (player_choice == S) {
-    if (computer_choice == "P") // win
+  if (player_choice == 'S') {
+    if (computer_choice == 'P') // win
     {
       cout << "Scissors cut Paper. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "L") // win
+    if (computer_choice == 'L') // win
     {
       cout << "Scissors decapitate Lizard. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "R") // loss
+    if (computer_choice == 'R') // loss
     {
       cout << "Rock crushes Scissors. You Lose..." << endl;
       return 1;
     }
-    if (computer_choice == "K") // loss
+    if (computer_choice == 'K') // loss
     {
       cout << "Spock smashes Scissors. You Lose..." << endl;
       return 1;
     } else // tie
     {
-      cout << "It's a tie.";
+      cout << "It's a tie." << endl;
       return 2;
     }
   }
-  if (player_choice == L) {
-    if (computer_choice == "K") // win
+  if (player_choice == 'L') {
+    if (computer_choice == 'K') // win
     {
       cout << "Lizard poisons Spock. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "P") // win
+    if (computer_choice == 'P') // win
     {
       cout << "Lizard eats Paper. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "S") // loss
+    if (computer_choice == 'S') // loss
     {
       cout << "Scissors decapitates Lizard. You Lose..." << endl;
       return 1;
     }
-    if (computer_choice == "R") // loss
+    if (computer_choice == 'R') // loss
     {
       cout << "Rock crushes Lizard. You Lose..." << endl;
       return 1;
     } else // tie
     {
-      cout << "It's a tie.";
+      cout << "It's a tie." << endl;
       return 2;
     }
   }
-  if (player_choice == K) {
-    if (computer_choice == "S") // win
+  if (player_choice == 'K') {
+    if (computer_choice == 'S') // win
     {
       cout << "Spock smashes Scissors. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "R") // win
+    if (computer_choice == 'R') // win
     {
       cout << "Spock vaporizes Rock. You win!" << endl;
       return 0;
     }
-    if (computer_choice == "P") // loss
+    if (computer_choice == 'P') // loss
     {
       cout << "Paper disproves Spock. You Lose..." << endl;
       return 1;
     }
-    if (computer_choice == "L") // loss
+    if (computer_choice == 'L') // loss
     {
       cout << "Lizard poisons Spock. You Lose..." << endl;
       return 1;
     } else // tie
     {
-      cout << "It's a tie.";
+      cout << "It's a tie." << endl;
       return 2;
     }
-  }
+  } else
+    cout << "this doesn't work. Please try again\n";
 }
 
 int main() {
@@ -195,7 +193,7 @@ int main() {
   cout << "Are you ready to play 'Rock, Paper, Scissors, Lizard, Spock?'"
        << endl;
   cout << "Enter: \nR for 'Rock' \nP for 'Paper \nS for 'Scissors' \nL for "
-          "'Lizard' \nK for 'Spock'";
+          "'Lizard' \nK for 'Spock'\n\n";
 
   for (i = 1; i <= 3; i++)
     a = game_loop();
@@ -210,6 +208,12 @@ int main() {
     ties++;
     break;
   }
-
+  
+  if (player_win > comp_win)
+    cout << "You beat the computer!" << endl;
+  else if (player_win < comp_win)
+    cout << "The computer beat you. . ." << endl;
+  else if (player_win == comp_win)
+    cout << "You tied the computer.";
   return 0;
 }
