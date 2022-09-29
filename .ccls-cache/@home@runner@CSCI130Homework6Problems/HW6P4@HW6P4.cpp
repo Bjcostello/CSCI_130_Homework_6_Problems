658f4e7ca5b4d6bdad56eba64a6df005495139fe
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-09-29 15:58:30 +0000

    HW6P4 not quite there. Entire switch statement prints for some reason

--- .ccls-cache/@home@runner@CSCI130Homework6Problems/HW6P4@HW6P4.cpp
+++ .ccls-cache/@home@runner@CSCI130Homework6Problems/HW6P4@HW6P4.cpp
@@ -13,10 +13,73 @@ int main() {
   cin >> students;
 
   for (students = students; students >= 1; students--) {
-    int date;
+    int date, a;
     string month;
     cin >> date;
-    month_converter(cin >> month);
+    cin >> month;
+    a = month_converter(month);
+    switch (a) {
+    case 1:
+      if (date <= 20)
+        cout << "Capricorn\n";
+      else
+        cout << "Aquarius\n";
+    case 2:
+      if (date <= 19)
+        cout << "Aquarius\n";
+      else
+        cout << "Pisces\n";
+    case 3:
+      if (date <= 20)
+        cout << "Pisces\n";
+      else
+        cout << "Aries\n";
+    case 4:
+      if (date <= 20)
+        cout << "Aries\n";
+      else
+        cout << "Taurus\n";
+    case 5:
+      if (date <= 20)
+        cout << "Taurus\n";
+      else
+        cout << "Gemini\n";
+    case 6:
+      if (date <= 21)
+        cout << "Gemini\n";
+      else
+        cout << "Cancer\n";
+    case 7:
+      if (date <= 22)
+        cout << "Cancer\n";
+      else
+        cout << "Leo\n";
+    case 8:
+      if (date <= 22)
+        cout << "Leo\n";
+      else
+        cout << "Virgo\n";
+    case 9:
+      if (date <= 21)
+        cout << "Virgo\n";
+      else
+        cout << "Libra\n";
+    case 10:
+      if (date <= 22)
+        cout << "Libra\n";
+      else
+        cout << "Scorpio\n";
+    case 11:
+      if (date <= 22)
+        cout << "Scorpio\n";
+      else
+        cout << "Sagittarius\n";
+    case 12:
+      if (date <= 21)
+        cout << "Sagittarius\n";
+      else
+        cout << "Capricorn\n";
+    }
   }
 
   return 0;
@@ -60,4 +123,5 @@ int month_converter(string month) { // converts text of month into int value
   if (month == "Dec") {
     return 12;
   }
+  return 0;
 }
