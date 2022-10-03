e8dfde260456eeb7cdd4e4c3d1a711c4c7e4094d
tattersol <106263649+Bjcostello@users.noreply.github.com>
2022-10-03 04:47:19 +0000

    Added breaks to switch statement to keep all statements in switch statement from running accidentally.

--- HW6P4/HW6P4.cpp
+++ HW6P4/HW6P4.cpp
@@ -7,6 +7,7 @@ using namespace std;
 
 // Functions***************************
 int month_converter(string month);
+
 // Main********************************
 int main() {
   int students;
@@ -16,69 +17,84 @@ int main() {
     int date, a;
     string month;
     cin >> date;
+    // cout<<date<<endl;
     cin >> month;
+    // cout<<month<<endl;
     a = month_converter(month);
+    // cout << a << endl;
     switch (a) {
     case 1:
       if (date <= 20)
         cout << "Capricorn\n";
       else
         cout << "Aquarius\n";
+      break;
     case 2:
       if (date <= 19)
         cout << "Aquarius\n";
       else
         cout << "Pisces\n";
+      break;
     case 3:
       if (date <= 20)
         cout << "Pisces\n";
       else
         cout << "Aries\n";
+      break;
     case 4:
       if (date <= 20)
         cout << "Aries\n";
       else
         cout << "Taurus\n";
+      break;
     case 5:
       if (date <= 20)
         cout << "Taurus\n";
       else
         cout << "Gemini\n";
+      break;
     case 6:
       if (date <= 21)
         cout << "Gemini\n";
       else
         cout << "Cancer\n";
+      break;
     case 7:
       if (date <= 22)
         cout << "Cancer\n";
       else
         cout << "Leo\n";
+      break;
     case 8:
       if (date <= 22)
         cout << "Leo\n";
       else
         cout << "Virgo\n";
+      break;
     case 9:
       if (date <= 21)
         cout << "Virgo\n";
       else
         cout << "Libra\n";
+      break;
     case 10:
       if (date <= 22)
         cout << "Libra\n";
       else
         cout << "Scorpio\n";
+      break;
     case 11:
       if (date <= 22)
         cout << "Scorpio\n";
       else
         cout << "Sagittarius\n";
+      break;
     case 12:
       if (date <= 21)
         cout << "Sagittarius\n";
       else
         cout << "Capricorn\n";
+      break;
     }
   }
 
